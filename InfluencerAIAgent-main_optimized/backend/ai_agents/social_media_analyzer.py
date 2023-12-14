507520f16c347487b97f36923581cf035a700f8d
Michael Gruen <michael.gruen9@gmail.com>
2023-12-13 20:20:37 -0500

    Update social_media_analyzer.py

--- InfluencerAIAgent-main_optimized/backend/ai_agents/social_media_analyzer.py
+++ InfluencerAIAgent-main_optimized/backend/ai_agents/social_media_analyzer.py
@@ -1,30 +1,30 @@
 from technology_stack.backend.database.mongodb import MongoDB
 
 class SocialMediaAnalyzer:
-    def __init__(self):
-        self.db = MongoDB()
-    
-    def analyze_hashtags(self, posts):
-        hashtag_counts = {}
-        for post in posts:
-            hashtags = post.get('hashtags', [])
-            for hashtag in hashtags:
-                if hashtag in hashtag_counts:
-                    hashtag_counts[hashtag] += 1
-                else:
-                    hashtag_counts[hashtag] = 1
-        return hashtag_counts
-    
-    def track_post_performance(self, posts):
-        post_performance = {}
-        for post in posts:
-            post_id = post.get('id')
-            likes = post.get('likes', 0)
-            comments = post.get('comments', 0)
-            shares = post.get('shares', 0)
-            post_performance[post_id] = {
-                'likes': likes,
-                'comments': comments,
-                'shares': shares
-            }
-        return post_performance
+   def __init__(self):
+       self.db = MongoDB()
+   
+   def analyze_hashtags(self, posts):
+       hashtag_counts = {}
+       for post in posts:
+           hashtags = post.get('hashtags', [])
+           for hashtag in hashtags:
+               if hashtag in hashtag_counts:
+                  hashtag_counts[hashtag] += 1
+               else:
+                  hashtag_counts[hashtag] = 1
+       return hashtag_counts
+   
+   def track_post_performance(self, posts):
+       post_performance = {}
+       for post in posts:
+           post_id = post.get('id')
+           likes = post.get('likes', 0)
+           comments = post.get('comments', 0)
+           shares = post.get('shares', 0)
+           post_performance[post_id] = {
+               'likes': likes,
+               'comments': comments,
+               'shares': shares
+           }
+       return post_performance
